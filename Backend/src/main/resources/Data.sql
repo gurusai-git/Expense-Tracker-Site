e9ef12e503c9f066dd5c8f58054e3a2a1c07f543
gurusai-git <115523670+gurusai-git@users.noreply.github.com>
2022-11-13 18:00:24 +0530

    Delete Backend directory

--- Backend/src/main/resources/Data.sql
+++
@@ -1,5 +0,0 @@
-insert into category values (1,'Travel');
-insert into category values (2,'Shopping');
-insert into category values (3,'Food');
-insert into category values(4,'Rent');
-insert into category values(5,'Groceries');
